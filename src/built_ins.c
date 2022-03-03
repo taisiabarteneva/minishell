8aa2dd21153473895156327c0883c00a246c96b8
Wahrks Urrigon <wurrigon@il-g5.msk.21-school.ru>
2022-03-03 18:20:10 +0300

    added a condition

--- src/built_ins.c
+++ src/built_ins.c
@@ -75,18 +75,39 @@ void execute_exit(void)
 
 }
 
+void execute_cd(t_env_vars **list, char *path)
+{
+	int status;
+	(void)list;
+	// if (!path)	
+	// 	path = ".";
+	
+	status = chdir(path);
+	if (status == -1)
+		write(STDERR_FILENO, "path is invalid\n", 17);
+}
+
 void built_ins(char *cmd, t_env_vars **list)
 {
 	// tmp argument
+	char	*path;
 	char 	*key;
 
 	key = "_";
-	if (ft_strncmp((const char *)cmd, "pwd", 3) == 0)
+	path = "/Users/wurrigon/Desktop";
+	if (ft_strncmp((const char *)cmd, "pwd", 3) == 0 &&
+		ft_strlen(cmd) == ft_strlen("pwd"))
 		execute_pwd(*list);
-	else if (ft_strncmp((const char *)cmd, "env", 3) == 0)
+	else if (ft_strncmp((const char *)cmd, "env", 3) == 0 &&
+		ft_strlen(cmd) == ft_strlen("env"))
 		execute_env(*list);
-	else if (ft_strncmp((const char *)cmd, "unset", 5) == 0)
+	else if (ft_strncmp((const char *)cmd, "unset", 5) == 0 &&
+		ft_strlen(cmd) == ft_strlen("unset"))
 		execute_unset(list, key);
-	else if (ft_strncmp((const char *)cmd, "exit", 4) == 0)
+	else if (ft_strncmp((const char *)cmd, "exit", 4) == 0 &&
+		ft_strlen(cmd) == ft_strlen("exit"))
 		execute_exit();
+	else if (ft_strncmp((const char *)cmd, "cd", 2) == 0 &&
+		ft_strlen(cmd) == ft_strlen("cd"))
+		execute_cd(list, path);		
 }
